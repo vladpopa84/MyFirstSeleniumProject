4f1aef78cbc20d4346c90f00343de651887d4783
Vlad Popa <vladpopa84@yahoo.com>
2019-11-18 19:50:52 +0200

    .

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+
 public class LoginTest {
 
     public static void loginTest() {
